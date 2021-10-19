7e5b374bd19b7bed9a2e84c7fb07834f085e2a54
priyan <45042030+priyan1995@users.noreply.github.com>
2021-10-19 22:17:00 +0530

    radio input added

--- src/components/FormikControl.js
+++ src/components/FormikControl.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Input } from "../form-fields/Input"
+import { RadioInput } from '../form-fields/RadioInput'
 
 export const FormControl = (props) =>{
     const { control, ...rest } = props
 
     switch (control){
         case 'input' : return <Input {...rest} />
+        case 'radio' : return <RadioInput {...rest} />
         default : return null
     }
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -8,15 +8,22 @@ export const Register = () => {
     const initialValues = {
         email: '',
         password: '',
-        confirmPassword: ''
+        confirmPassword: '',
+        modeOfContact: ''
     }
+
+    const modesOfCont = [
+        { key: 'Email', value: 'emailtype' },
+        { key: 'Telephone', value: 'telephoneype' }
+
+    ]
 
     const validationSchema = Yup.object({
         email: Yup.string().email('Invalid email format').required('Email is required'),
         password: Yup.string().required('Password is Required'),
         confirmPassword: Yup.string().oneOf([Yup.ref('password'), ''], 'Password Should Matched').required('Confirm the password')
 
-    })   
+    })
 
     const onSubmit = (values) => {
         console.log('Form Data ', values);
@@ -33,25 +40,32 @@ export const Register = () => {
                     {
                         formik => (
                             <Form>
-                                <FormControl 
-                                control='input' 
-                                type='email'
-                                label='Email' 
-                                name='email'/>
+                                <FormControl
+                                    control='input'
+                                    type='email'
+                                    label='Email'
+                                    name='email' />
 
                                 <FormControl
-                                control='input'
-                                type='password'
-                                label='Password'
-                                name='password'
+                                    control='input'
+                                    type='password'
+                                    label='Password'
+                                    name='password'
                                 />
 
 
-                                <FormControl 
-                                control='input'
-                                type='password'
-                                label='Confirm Password'
-                                name='confirmPassword'
+                                <FormControl
+                                    control='input'
+                                    type='password'
+                                    label='Confirm Password'
+                                    name='confirmPassword'
+                                />
+
+                                <FormControl
+                                    control='radio'
+                                    label='Mode of Contact'
+                                    name='modeOfContact'
+                                    options={modesOfCont}
                                 />
 
                                 <button type='submit'>Submit</button>
